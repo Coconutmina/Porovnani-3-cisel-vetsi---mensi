f7d7d242e87c31f303114a5edc28191a502e58ec
matěj <matj@DESKTOP-1HI5AKA>
2021-10-31 18:28:01 +0100

    Přidejte soubory projektu.

---
+++ Porovnání 3 čísel větší - menší/Porovnání 3 čísel větší - menší.cpp
@@ -0,0 +1,29 @@
+﻿//Největší číslo ze tří
+#include <stdio.h>
+
+int main()
+{
+	//čísla dosazená do vzorce
+	int a = 5, b = 8, c = 10;
+	int max;
+	//Postupné vzorce
+	//mimochodem tento vzorec jsem si našel na internetu, protože jsem ze začátku nevěděl jak se to dělá tak doufám že to nevadí.
+	if (a == b == c) {
+		max = a;
+	}
+	else {
+		if ((a > b) && (a > c)) {
+			max = a;
+		}
+		else {
+			if ((b > a) && (b > c)) {
+				max = b;
+			}
+			else {
+				max = c;
+			}
+		}
+	}
+	//Finální věta dosazená do vzorce
+	printf("nejvetsí cislo ze vsech je %d", max);
+}
